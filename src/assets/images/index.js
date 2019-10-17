ed179c266ce9c24e58a6a3c7f5f3eb8c67f09288
Aichnerc <contact@aichner-christian.com>
2019-10-18 00:42:53 +0200

    Improve Gallery
    More images and detailed information has been
    added.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,66 +1,422 @@
+// Main images
 import img00 from './0.jpg';
 import img01 from './1.jpg';
-import img02 from './2.jpg';
-import img03 from './3.jpg';
-import img04 from './4.jpg';
-import img05 from './5.jpg';
-import img06 from './6.jpg';
-import img07 from './7.jpg';
-import img08 from './8.jpg';
-import img09 from './9.jpg';
-import img10 from './10.jpg';
 import img11 from './11.jpg';
 import img12 from './12.jpg';
 import img13 from './13.jpg';
-import img14 from './14.jpg';
 import img15 from './15.jpg';
 import img16 from './16.jpg';
 import img17 from './17.jpg';
 import img18 from './18.jpg';
 import img19 from './19.jpg';
-import img20 from './20.jpg';
 import img21 from './21.jpg';
-import img22 from './22.jpg';
 import img23 from './23.jpg';
 import img24 from './24.jpg';
-import img25 from './25.jpg';
-import img26 from './26.jpg';
-import img27 from './27.jpg';
-import img28 from './28.jpg';
-import img29 from './29.jpg';
-import img30 from './30.jpg';
-import img31 from './31.jpg';
-import img32 from './32.jpg';
+// Off images
+import img0001 from './off/1.jpg';
+import img0002 from './off/2.jpg';
+import img0003 from './off/3.jpg';
+import img0004 from './off/4.jpg';
+import img0005 from './off/5.jpg';
+import img0006 from './off/6.jpg';
+import img0007 from './off/7.jpg';
+import img0008 from './off/8.jpg';
+import img0009 from './off/9.jpg';
+import img0010 from './off/10.jpg';
+import img0011 from './off/11.jpg';
+import img0012 from './off/12.jpg';
+// Off2 images
+import img001 from './off2/1.jpg';
+import img002 from './off2/2.jpg';
+import img003 from './off2/3.jpg';
+import img004 from './off2/4.jpg';
+import img005 from './off2/5.jpg';
+import img006 from './off2/6.jpg';
+import img007 from './off2/7.jpg';
 
 const images = [
     { 
         url: img00, 
-        title: "HELVEGEN : | Laukr - Hugin und ein Meer von Regen", 
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar elit mi, et faucibus 
-        risus pulvinar nec. Phasellus ut hendrerit nulla. Sed tincidunt libero urna, vitae porta nisi 
-        imperdiet non. Duis imperdiet ligula eget quam commodo, at convallis ipsum cursus. Orci varius 
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.`, 
+        title: ": HELVEGEN : | Laukr - Hugin und ein Meer von Regen", 
+        text: `So wie Odin sein Auge für mehr Wissen opferte, symbolisiert dieses Bild 
+        den Tod alter Gedanken - die bewusste Aufopferung um Platz für Neues zu schaffen.
+        Deswegen sehen wir auch die umgekehrte Algir-Rune auf der Stirn, die nun nicht mehr 
+        das Leben sondern den Tod symbolisiert. Im Hintergrund sehen wir, wie im Titel bereits 
+        erwähnt ein Meer von Regen - eine poetische Bezeichnung für den Sommer, der oft von vielen
+        Gewittern geprägt wird. Also zeigt uns das Bild den ersten Tag des Herbstes und den Anfang von
+        Helvegen (altnordische Bezeichnung für den Weg des Todes). Im Herbst verlieren die Bäume die Blätter,
+        um im Frühling neu zu erblühen. Im Titel wird auch Odins Rabe Hugin erwähnt, der nichts anderes als 
+        Gedanken bedeutet.`, 
         available: false, 
         details: {
-            dimensions: "20x20",
-            weight: "2,5",
-            date: "19.01.2019"
+            dimensions: "100 x 80",
+            material: "Ölfarbe, Ölkreide, Farbspray, Kohlestifte auf Leinwand",
+            date: "September 2019"
         }
     },
     { 
         url: img01, 
-        title: "HELVEGEN : | Laukr - Hugin und ein Meer von Regen", 
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar elit mi, et faucibus 
-        risus pulvinar nec. Phasellus ut hendrerit nulla. Sed tincidunt libero urna, vitae porta nisi 
-        imperdiet non. Duis imperdiet ligula eget quam commodo, at convallis ipsum cursus. Orci varius 
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.`,
-        available: true, 
+        title: "HELVEGEN : | Algiz - Gebunden", 
+        text: `Diese Bild symbolisiert die Gebundenheit an den Kreislauf des Lebens. Wir sehen wie ein Mann an die
+        Algiz-Rune (Lebensrune) gebunden ist, während uns der Hintergrund und die Farbwahl an einen Sonnenuntergang
+        erinnern soll. Die Runenkombination auf dem Baumstamm steht für die innere Kraft und Stärke sein Schicksal
+        selbst zu lenken, während die Rune auf der Brust des Mannens nichts anderes ist als der Wille durch eine 
+        schwere Zeit durchzuhauen, mit dem Versprechen gebunden dadurch zu wachsen.`, 
+        available: false, 
         details: {
-            dimensions: "20x20",
-            weight: "2,5",
-            date: "19.01.2019"
+            dimensions: "100 x 80",
+            material: "Ölfarbe, Farbspray, Kohle auf schwere Holzplatte",
+            date: "Oktober 2019"
         }
     },
+    { 
+        url: img19, 
+        title: ": HELVEGEN : | Laukr - Das Geräusch von Wasser / Selbstbildnis", 
+        text: `Dieses Bild zeigt uns den Weg zu Hel. Die Farbwahl soll an den Herbst und die Jahresdämmerung 
+        erinnern.`, 
+        available: true, 
+        details: {
+            dimensions: "60 x 60",
+            material: "Ölfarbe, Ölkreide, Farbspray auf Leinwand",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img15, 
+        title: ": VINDAVLA | Vindavla", 
+        text: `Das Bild thematisiert das Erlangen von Wissen und die Erweiterung des Verstands. Gezeigt wird Odin,
+        der nachdem er sich selbst das Leben nahm - um zu erfahren wie es ist zu sterben - nun hier neu erwacht. 
+        Auf seiner Stirn sehen wir die Rune für Tapferkeit. Die rote Farbe symbolisiert den Wind und das Leben.`, 
+        available: false, 
+        details: {
+            dimensions: "110 x 80",
+            material: "Ölfarbe, Kohle auf Holzplatte",
+            date: "Juni 2019"
+        }
+    },
+    { 
+        url: img13, 
+        title: ": VINDAVLA | Alter Mann", 
+        text: `Frühlingskind\nim Morgenrot der Jahre\nverflochten im Wald\nvergessener Tage\n\nAlter Mann\nWann 
+        fängt das Leben an\nSind wir gebunden an Zeit\noder nur getrieben von Leid\n\nFrühlingskind\ndurchströmt 
+        von Wind\nZeit wird dich jagen\nund Leit wirst du tragen\n\nAlter Mann\ngeformt von Kälte\ndu hängst am 
+        Stamm\nunter der Sterne Himmelszelte`, 
+        runes: true,
+        available: true, 
+        details: {
+            dimensions: "110 x 80",
+            material: "Ölfarbe, Ölkreide, Kohle auf Holzplatte",
+            date: "April 2019"
+        }
+    },
+    { 
+        url: img21, 
+        title: ": VINDAVLA | Mein Sonnenschein", 
+        text: `Das Bild zeigt die Idee eines Frühlings - der erste Sonnenstrahl nach einem langen, kalten Winter. 
+        Ein Ausbruch eines Gedankens - der Ausdruck des Lebens. Ein Prozess der ähnlich wie eine Geburt schmerzhaft 
+        ist. Das Leben hat eine schwere Geburt.`, 
+        available: true, 
+        details: {
+            dimensions: "70 x 60",
+            material: "Ölfarbe, Kohle auf Leinwand",
+            date: "März 2019"
+        }
+    },
+    { 
+        url: img16, 
+        title: ": Die kalte Wand | Schlagschatten", 
+        text: `Herzen schalgen gegen die Wand\nund Schatten gegen den Verstand.\n\nIch träume,\nvoller Bedauern\n
+        von Sonnenschein\nhinter kalten Mauern.\n\nIch träume\nbis der Wind wie Licht,\ndie Kälte durchbricht.\n
+        Bis das Leben erwacht,\nbis ans Ende der Nacht.\n\nO mein Sonnenschein,\ndas Herz überflutet.\nOh mein
+        Sonnenschein,\nder Verstand blutet.`, 
+        runes: true,
+        available: false, 
+        details: {
+            dimensions: "70 x 60",
+            material: "Weißer Buntstift, schwarze Tusche auf schwarzem Papier",
+            date: "Jänner 2019"
+        }
+    },
+    { 
+        url: img11, 
+        title: ": Die kalte Wand | Nur Zorn und Verständnislosigkeit / Selbstbildnis", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "70 x 60",
+            material: "Weißer Lack, Buntstift auf schwarzem und weißen Papier",
+            date: "Dezember 2018"
+        }
+    },
+    { 
+        url: img12, 
+        title: ": Die kalte Wand | Die Sorgen um Mumin oder das Versprechen der Bäume an den kalten Weg der Täume", 
+        text: `Bäume schreien gegen die Zeit\nund Vögel der Sonne hinterher.\nSchweigend,\nwie ein Blatt im Wind 
+        tanzend\nschwinden Gedanken an Erinnerungen.\n\nVerschwinde\nund verlier dich in Vergessenheit.\nVerliere\n
+        und halte dein Versprechen.\n\nFolge kalten Träumen\nund träume von weißer Kälte.\nVerschwinde in der kalten 
+        Wand\nUnd verlier dich in dir.\n\nDoch hinter müden AUgen\nund kalten Wänden\nnur Zorn und 
+        Verständnislosigkeit.\nNur Zorn und Verständnislosigkeit.`, 
+        available: true, 
+        runes: true,
+        details: {
+            dimensions: "70 x 60",
+            material: "Kohle auf weißem Papier",
+            date: "November 2018"
+        }
+    },
+    { 
+        url: img17, 
+        title: ": Herbstschreierei | Munin / Selbstbildnis", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "40 x 30",
+            material: "Öl auf Leinwand",
+            date: "November 2018"
+        }
+    },
+    { 
+        url: img18, 
+        title: ": Herbstschreierei | Tapferkeit", 
+        text: `Ein Mann hört die Bäume sterben.`, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "Oktober 2018"
+        }
+    },
+    { 
+        url: img24, 
+        title: ": Herbstschreierei | Naturschlag", 
+        text: `Ein Mann hört die Bäume sterben.`, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "Oktober 2018"
+        }
+    },
+    { 
+        url: img23, 
+        title: ": Herbstschreierei | Helvegen", 
+        text: `Ein Mann hört die Bäume sterben.`, 
+        available: true, 
+        details: {
+            dimensions: "40 x 30",
+            material: "Öl auf Leinwand",
+            date: "Oktober 2018"
+        }
+    },
+    { 
+        url: img001, 
+        title: "Der Frühling kommt", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "40 x 30",
+            material: "Öl auf Leinwand",
+            date: "März 2019"
+        }
+    },
+    { 
+        url: img002, 
+        title: "Licht", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "März 2019"
+        }
+    },
+    { 
+        url: img003, 
+        title: "Wind", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "März 2019"
+        }
+    },
+    { 
+        url: img004, 
+        title: "Frühlingsgedanken", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "40 x 30",
+            material: "Öl auf Leinwand",
+            date: "Mai 2019"
+        }
+    },
+    { 
+        url: img005, 
+        title: "Ein Meer von Regen", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "Juni 2019"
+        }
+    },
+    { 
+        url: img006, 
+        title: "Blutkreislauf", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "60 x 50",
+            material: "Öl auf Leinwand",
+            date: "Jänner 2019"
+        }
+    },
+    { 
+        url: img007, 
+        title: "Munin", 
+        text: ``, 
+        available: false, 
+        details: {
+            dimensions: "60 x 60",
+            material: "Ölfarbe, Ölkreide auf Leinwand",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0001, 
+        title: "Gedanken fallen IV", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölfarbe, Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0002, 
+        title: "Gedanken fallen V", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölfarbe, Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0003, 
+        title: "Gedanken fallen I", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0004, 
+        title: "Sommerträume IV", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    },
+    { 
+        url: img0005, 
+        title: "Sommerträume II", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    },
+    { 
+        url: img0006, 
+        title: "Sommerträume III", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    },
+    { 
+        url: img0007, 
+        title: "Sommerträume I", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    },
+    { 
+        url: img0008, 
+        title: "Gedanken fallen VI", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölfarbe, Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0009, 
+        title: "Gedanken fallen III", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0010, 
+        title: "Gedanken fallen II", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "50 x 30",
+            material: "Ölkreide auf Papier",
+            date: "September 2019"
+        }
+    },
+    { 
+        url: img0011, 
+        title: "Selbstbildnis", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    },
+    { 
+        url: img0012, 
+        title: "Selbstbildnis", 
+        text: ``, 
+        available: true, 
+        details: {
+            dimensions: "30 x 20",
+            material: "Kohle auf Papier",
+            date: "Juli 2019"
+        }
+    }
 ]
 
 export default images;
